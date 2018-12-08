80dbeba686261ff22190fbb690b2972a9730bb09
2164165 <37785711+2164165@users.noreply.github.com>
2018-12-08 13:25:02 +0800

    add programs

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main{
+	static Scanner s=new Scanner(System.in);
+	public static void main(String[] $){
+		long a=s.nextLong(),b=s.nextLong();
+		System.out.println((b+a-1)/a);
+	}
+}
---
+++ codeforce.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class codefore{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String s=sc.next();
+		int a=0,b,c;
+		for(int i=0;i<n;i++)
+		{
+			if(s.charAt(i)=='8')
+				a++;
+		}
+		b=n/11;
+		if(a<b)
+			System.out.println(a);
+		else
+			System.out.println(b);
+	}
+}
+	
